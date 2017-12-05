b7f356624eab7963a2ddc79fa1719a29ba61e55c
Kendall <nicho147@gmail.com>
2017-12-05 10:46:02 -0500

    maze game in terminal

---
+++ src/Maze.java
@@ -0,0 +1,180 @@
+/*
+* This class creates a graph that serves as the maze, maze is just random, so to find
+* a solvable maze the solvable boolean serves as a recursive method to do so  and to then
+* display the maze the display function will format a string output
+* */
+
+import java.util.ArrayList;
+
+public class Maze {
+    private Graph maze;
+    private Graph fancyMaze;
+    private int bound;
+    private int current;
+    private int end;
+    private int initial = 0;
+
+    public Maze(int bound){
+        this.bound = bound;
+        maze = new Graph(bound);
+        fancyMaze = maze;
+        //System.out.println("num edges at construct: "+maze.getEdges().size());
+        //System.out.println("valid edges at construct: " + maze.getValidEdges().size());
+        current = 0;
+        //start = 0;
+        end = (bound*bound)-1;
+    }
+
+    //used for tracking solvable when testing
+    public void spit(ArrayList<Edge> stuff){
+        for (int i = 0; i < stuff.size(); i++) {
+            System.out.println("start:" + stuff.get(i).getStart());
+            System.out.println("end:" + stuff.get(i).getEnd());
+            System.out.println("weight:" + stuff.get(i).getWeight());
+            System.out.println("block:" + stuff.get(i).getBlock());
+        }
+    }
+
+    //for game play
+    public boolean play(int move){
+        int nextLoc = initial+move;
+        if(nextLoc >= 0 && nextLoc < (bound*bound)){
+            if(!maze.findBlock(initial,nextLoc)){
+                initial = nextLoc;
+                if(initial == end){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //uses recursion to find if maze if solvable
+    //outputs true if it can reach end
+    //outputs false it end cannot be reached
+    public boolean solvable() {
+        int index;
+        ArrayList<Edge> duplicate = maze.getValidEdges();
+        ArrayList<Edge> options = maze.getOptions(duplicate,current);
+        while (options.size() > 0) {
+            //for testing
+            //spit(options);
+            index = maze.lightest(options);
+            duplicate.remove(options.get(index));
+
+            if (options.get(index).getEnd() == end) {
+                return true;
+            }
+            else {
+                current = options.get(index).getEnd();
+                options.remove(options.get(index));
+                return solvable();
+            }
+
+        }
+        //System.out.println("C'est impossible");
+        return false;
+    }
+
+    //formats a maze display and prints
+    public void displayMaze(){
+        //System.out.println("num edges at display : "+maze.getEdges().size());
+       // System.out.println("fancy num edges at display : "+fancyMaze.getEdges().size());
+        //System.out.println(initial);
+        String display = "";
+        String hLine = "";
+        String vLine = "";
+//        for(int b = 0; b<bound; b++) {
+//            for (int p = 0; p < bound; p++) {
+//                System.out.print(maze.getHorizontal().getBlocks()[b][p]);
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+//        for(int b = 0; b<bound; b++){
+//            for (int p = 0; p < bound; p++) {
+//                System.out.print(maze.getVertical().getBlocks()[b][p]);
+//            }
+//            System.out.println();
+//        }
+
+        for(int k =0; k<bound; k++){
+            //display += String.format("%1c"+"%1c",' ','_');
+            display += " ---";
+        }
+        display += String.format("\n");
+        for(int i = 0; i<bound; i++){
+            for(int j =0; j<bound; j++){
+//                if(j==0){
+//                    display += String.format("|");
+//                }
+//                if(maze.getHorizontal().getBlocks()[i][j] && maze.getVertical().getBlocks()[i][j]) {
+//                    display += String.format("%1c"+"%1c",'_','|');
+//                }
+//                else if(maze.getHorizontal().getBlocks()[i][j] && !maze.getVertical().getBlocks()[i][j]) {
+//                    display += String.format("%1c"+"%1c",'_',' ');
+//               }
+//                else if(!maze.getHorizontal().getBlocks()[i][j] && maze.getVertical().getBlocks()[i][j]) {
+//                    display += String.format("%1c"+"%1c",' ','|');
+//                }
+//                else if(!maze.getHorizontal().getBlocks()[i][j] && !maze.getVertical().getBlocks()[i][j]) {
+//                    display += String.format("%1c"+"%1c",' ',' ');
+//                }
+
+/////////////////////////////////////
+//new larger maze display
+                if(j==0){
+                    display += String.format("|");
+                }
+                if(maze.getHorizontal().getBlocks()[i][j] && maze.getVertical().getBlocks()[i][j]) {
+                    if(i == initial/bound && j == initial%bound){
+                        vLine += " O |";
+                        hLine += " ---";
+                    }
+                    else {
+                        vLine += "   |";
+                        hLine += " ---";
+                    }
+                }
+                else if(maze.getHorizontal().getBlocks()[i][j] && !maze.getVertical().getBlocks()[i][j]) {
+                    if(i == initial/bound && j == initial%bound){
+                        vLine += " O  ";
+                        hLine += " ---";
+                    }
+                    else {
+                        vLine += "    ";
+                        hLine += " ---";
+                    }
+               }
+                else if(!maze.getHorizontal().getBlocks()[i][j] && maze.getVertical().getBlocks()[i][j]) {
+                    if(i == initial/bound && j == initial%bound){
+                        vLine += " O |";
+                        hLine += "    ";
+                    }
+                    else {
+                        vLine += "   |";
+                        hLine += "    ";
+                    }
+                }
+                else if(!maze.getHorizontal().getBlocks()[i][j] && !maze.getVertical().getBlocks()[i][j]) {
+                    if(i == initial/bound && j == initial%bound){
+                        vLine += " O  ";
+                        hLine += "    ";
+                    }
+                    else {
+                        vLine += "    ";
+                        hLine += "    ";
+                    }
+                }
+
+            }
+            display += vLine + "\n";
+            display += hLine + "\n";
+            vLine = "";
+            hLine = "";
+            //display += String.format("\n");
+        }
+        System.out.println(display);
+        //System.out.println("woah");
+    }
+}
---
+++ src/MazeToTheMax.java
@@ -0,0 +1,88 @@
+/*
+*
+* */
+
+import java.util.*;
+
+public class MazeToTheMax {
+
+    private int bound;
+    private int levelNum;
+    private boolean pass = false;
+    private Random r = new Random();
+    private Maze level;
+    private String move;
+    private Scanner sc = new Scanner(System.in);
+
+
+    public MazeToTheMax(){
+        levelNum = 1;
+        bound = 25;
+        move = "";
+        levelUp();
+    }
+
+    public int getLevel(){
+        return levelNum;
+    }
+
+/*
+* implement a while loop to generate mazes until pass turns false
+* pass = false if player cannot complete level
+* can place limit on maze size by limiting bound in while() (while(pass && bound < 30))
+* to help with the lag when finding solvable mazes of large bounds
+* */
+    public void levelUp(){//dictates maze bounds or level
+        while(bound<35){
+            System.out.println("THIS IS LEVEL " + levelNum + "!!!");
+            //System.out.println("bound: " + bound);
+            level = new Maze(bound);
+            //level.displayMaze();
+            boolean solvable = level.solvable();
+
+            while(!solvable){
+                level = new Maze(bound);
+                //level.displayMaze();
+                solvable = level.solvable();
+            }
+            level.displayMaze();
+
+            while(!pass){
+                System.out.println("use commands a(left), d(right), w(up), and s(down)");
+                move = sc.next();
+                switch (move){
+                    case "a":
+                        pass = level.play(-1);
+                        break;
+                    case "s":
+                        pass = level.play(bound);
+                        break;
+                    case "d":
+                        pass = level.play(1);
+                        break;
+                    case "w":
+                        pass = level.play(-bound);
+                        break;
+                    default:
+                        System.out.println("sorry that isn't an input option");
+                        pass = false;
+                        break;
+                }
+                level.displayMaze();
+            }
+
+            System.out.println("yeet boi");
+
+            pass = false;
+            levelNum++;
+            bound++;
+        }
+    }
+
+
+
+    static public void main(String args[]){
+        MazeToTheMax mm = new MazeToTheMax();
+        System.exit(0);
+    }
+}
